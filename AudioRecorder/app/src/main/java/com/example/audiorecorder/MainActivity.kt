0b981616abecc922714c7d398330c563caacf7c5
Jiří Klimeš <it.klimes@gmail.com>
2022-12-14 16:43:43 +0100

    Saving files + rename finished

--- AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.audiorecorder
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
+import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
 const val REQUEST_CODE = 200
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
     private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var permissionGranted = false
@@ -24,17 +34,25 @@ class MainActivity : AppCompatActivity() {
     private var isRecording = false
     private var isPaused = false
 
+    private lateinit var timer: Timer
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
         permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
 
         if (!permissionGranted)
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
 
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight = 0
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        timer = Timer(this)
 
         btnRecord.setOnClickListener {
             when{
@@ -43,6 +61,66 @@ class MainActivity : AppCompatActivity() {
                 else -> startRecording()
             }
         }
+
+        btnList.setOnClickListener {
+            //  TODO
+            Toast.makeText(this, "List button", Toast.LENGTH_SHORT).show()
+
+        }
+
+        btnDone.setOnClickListener {
+            stopRecorder()
+            //Toast.makeText(this, "Record saved", Toast.LENGTH_SHORT).show()
+
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBG.visibility = View.VISIBLE
+            filenameInput.setText(fileName)
+        }
+
+        btnCancel.setOnClickListener{
+            File("$dirPath$fileName.mp3").delete()
+            dismiss()
+        }
+
+        btnOk.setOnClickListener{
+            dismiss()
+            save()
+        }
+
+        bottomSheetBG.setOnClickListener {
+            File("$dirPath$fileName.mp3").delete()
+            dismiss ()
+        }
+
+        btnDelete.setOnClickListener {
+           stopRecorder()
+            File("$dirPath$fileName.mp3").delete()
+            Toast.makeText(this, "Record deleted", Toast.LENGTH_SHORT).show()
+        }
+
+        btnDelete.isClickable = false
+    }
+
+    private fun save(){
+        val newFileName = filenameInput.text.toString()
+        if(newFileName != fileName){
+            var newFile = File("$dirPath$newFileName.mp3")
+            File("$dirPath$newFileName.mp3").renameTo(newFile)
+        }
+    }
+
+    private fun dismiss(){
+        bottomSheetBG.visibility = View.GONE
+        hideKeyboard(filenameInput)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }, 100)
+    }
+
+    private fun hideKeyboard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     override fun onRequestPermissionsResult(
@@ -60,20 +138,24 @@ class MainActivity : AppCompatActivity() {
         recorder.pause()
         isPaused = true
         btnRecord.setImageResource(R.drawable.ic_record)
+
+        timer.pause()
     }
 
     private fun resumeRecorder(){
         recorder.resume()
         isPaused = false
         btnRecord.setImageResource(R.drawable.ic_pause)
+
+        timer.start()
     }
 
+    @Suppress("DEPRECATION")
     private fun startRecording(){
         if (!permissionGranted){
              ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
              return
         }
-        //start recording
 
         recorder = MediaRecorder()
         dirPath = "${externalCacheDir?.absolutePath}/"
@@ -93,14 +175,45 @@ class MainActivity : AppCompatActivity() {
             } catch (e: IOException){
 
             }
-
             start()
-
         }
-
         btnRecord.setImageResource(R.drawable.ic_pause)
         isRecording = true
         isPaused = false
+
+        timer.start()
+
+        btnDelete.isClickable = true
+        btnDelete.setImageResource(R.drawable.ic_delete)
+
+        btnList.visibility = View.GONE
+        btnDone.visibility = View.VISIBLE
+    }
+
+    private fun stopRecorder(){
+        timer.stop()
+
+        recorder.apply {
+            stop()
+            release()
+        }
+
+        isPaused = false
+        isRecording = false
+
+        btnList.visibility = View.VISIBLE
+        btnDone.visibility = View.GONE
+
+        btnDelete.isClickable = false
+        btnDelete.setImageResource(R.drawable.ic_delete_disabled)
+
+        btnRecord.setImageResource(R.drawable.ic_record)
+
+        tvTimer.text = "00:00.00"
+    }
+
+    override fun onTimerTick(duration: String) {
+        tvTimer.text = duration
     }
 
 }
